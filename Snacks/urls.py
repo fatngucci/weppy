6de6faa0ce6fafe3b7dc0e0e164fc9b01c965aba
s85524 <s85524@bht-berlin.de>
2022-11-30 15:21:54 +0100

    urls, views

---
+++ Snacks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('show/', views.snacks_list, name='snack-list'),
+    path('show/<int:pk>/', views.snacks_detail, name='snack-detail'),
+    path('show/<int:pk>/delete', views.snacks_delete, name='snack-delete'),
+    path('add/', views.snacks_create, name='snack-create'),
+]
--- Snacks/views.py
+++ Snacks/views.py
@@ -9,3 +9,40 @@ def snacks_list(request):
     context = {'all_the_snacks': all_the_snacks}
     return render(request, 'snack-list.html', context)
 
+def snacks_detail(request, **kwargs):
+    snack_id = kwargs['pk']
+    that_one_snack = Snack.objects.get(id=snack_id)
+
+    context = {'that_one_snack': that_one_snack,
+               }
+
+    return render(request, 'snack-detail.html', context)
+
+def snacks_create(request):
+    if request.method == 'POST':
+        form = SnackForm(request.POST)
+        form.instance.hersteller = request.user
+        if form.is_valid():
+            form.save()
+        else:
+            pass
+
+        return redirect('snack-list')
+
+    else:
+        form = SnackForm()
+        context = {'form': form}
+        return render(request, 'snack-create.html', context)
+
+def snacks_delete(request, **kwargs):
+    snack_id = kwargs['pk']
+    to_be_deleted = Snack.objects.get(id=snack_id)
+    context = {'that_one_snack': to_be_deleted}
+
+    if request.method == 'POST':
+        if 'cancel' in request.POST:
+            return redirect('snack-list')
+        to_be_deleted.delete()
+        return redirect('snack-list')
+
+    return render(request, 'snack-delete.html', context)
